d385bdaf8548fb1f575702331433e88722948d8c
Zachary A. Rodgers <zarodgers@lab-mcsp-24.academic.roanoke.edu>
2017-04-10 08:57:44 -0400

    Zach Stat

--- app/controllers/StatController.php
+++ app/controllers/StatController.php
@@ -7,5 +7,21 @@ class StatController extends Controller {
 		return View::make('hello');
 	}
 
+  public function mean($an_array){
+      $sum = 0;
+      for ($index = 0; $index < count($an_array); $index++){
+          $sum = $sum + an_array[$index];
+      }
+      return $sum/count($an_array);
+  }
 
+  public function std_dev($an_array){
+      $sum = 0;
+      $mean = mean($an_array);
+      for ($index = 0; $i < count($an_array); $index++) {
+          $sum = $sum + (an_array[$index] - $mean)**2;
+      }
+      return (sqrt($sum /(count($an_array) - 1)));
+  }
+  
 }
