47912160dd80e0648113f3370acd36e34682e1a8
Tyler Strouth <tcstrouth@lab-mcsp-04.academic.roanoke.edu>
2017-04-12 10:01:34 -0400

    added php function to app/controllers/StatController.php and added get_pval.py to app/controllers

--- app/controllers/StatController.php
+++ app/controllers/StatController.php
@@ -2,10 +2,47 @@
 
 class StatController extends Controller {
 
-  public function showWelcome()
+    public function showWelcome()
 	{
 		return View::make('hello');
 	}
 
+    //needs an array and inputted test statisitc executes a python program and
+    //returns the probability value  
+    public function retrievepval($pArray, $teststat){
+        $df = len($pArray) - 1;
+        $stringtt = sprintf("%5f", $teststat);
+        $stringdf = sprintf("%1f", $df);
+        $pval = exec('python3 getpval.py '.$stringtt.' '. $stringdf);
+        return $pval;
+    }
 
+   //inputs an array and a value from null hypothesis inputted by user
+   //calculates the test statistic
+    public function getteststat($pArray, $nullmean){
+        $tt = (mean($pArray) - $nullmean)/(sd($pArray)/sqrt(len($pArray)));
+        return $tt;
+    }
+
+   //needs an inputted array of population data
+   //calculates the confidence interval of the population data
+    public function getconfinterval($pArray){
+        $standard_deviation_div_sqrtn = (sd($pArray)/sqrt(len($pArray)));
+        $tt = (mean($pArray) - $nullmean)/$standard_deviation_div_sqrtn;
+        $mean = mean($pArray);
+        $lowerbound = $mean - $tt*$standard_deviation_div_sqrtn;
+        $upperbound = $mean + $tt*$standard_deviation_div_sqrtn;
+        echo ("the confidence interval is [",$lowerbound,
+        " , "$upperbound, "] \n");
+    }
+
+    public function main($pArray, $nullmean){
+        $teststat = getteststat($pArray, $nullmean=0);
+        echo ("the test statistic is ",$teststat,"\n"); //prints test stat
+        getconfinterval($pArray); //prints confidence interval
+        echo ("p value ",retrievepval($pArray, $teststat),"\n");
+        //prints p value
+    }
 }
+
+
