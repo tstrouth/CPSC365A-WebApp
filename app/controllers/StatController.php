68a6e0aeac288e5a506fd7674d0fd1d2aaeb2849
Tyler Strouth <tcstrouth@lab-mcsp-04.academic.roanoke.edu>
2017-04-17 10:12:29 -0400

    rearranged php in Statcontroller

--- app/controllers/StatController.php
+++ app/controllers/StatController.php
@@ -11,49 +11,45 @@ class StatController extends Controller {
     //returns the probability value
     function retrievepval($pArray, $teststat){
         $df = count($pArray) - 1;
-        $stringtt = sprintf("%f", $teststat);
-        $stringdf = sprintf("%f", $df);
-        exec('python3 get_pval.py '.$stringtt.' '. $stringdf, $pval);
-        echo sprintf(" and the p value is %f \n", $pval[0]);
+        $command = sprintf("python3 get_pval.py %f %f", $teststat, $df);
+        exec($command, $pval);
+        return $pval[0];
     }
 
-    //echo "passed first function \n";
     //inputs an array and a value from null hypothesis inputted by user
     //calculates the test statistic
     function getteststat($pArray, $nullmean){
-        //echo 'in the function';
         $n = count($pArray);
         $tt = (mean($pArray) - $nullmean)/(std_dev($pArray)/sqrt($n));
         return $tt;
     }
 
-    //echo "\n passed second function";
 
     //needs an inputted array of population data
     //calculates the confidence interval of the population data
-    function getconfinterval($pArray){
+    function getconfinterval($pArray, $alpha){
         $standard_deviation_div_sqrtn = (std_dev($pArray)/sqrt(count($pArray)));
-        $stringdf = sprintf("%f", (count($pArray) - 1));
-        exec('python3 get_tval.py .025 '.$stringdf , $t);
+        $alpha_div_2 = $alpha/2;
+        $command = sprintf("python3 get_tval.py %f %f", $alpha_div_2,
+        count($pArray) - 1);
+        exec($command, $t);
         $mean = mean($pArray);
         $lowerbound = $mean - $t[0]*$standard_deviation_div_sqrtn;
         $upperbound = $mean + $t[0]*$standard_deviation_div_sqrtn;
-        echo "the confidence interval is [";
-        echo $lowerbound;
-        echo " , ";
-        echo $upperbound;
-        echo "] \n";
+        printf("the confidence interval is [ %f, %f]",
+        $lowerbound, $upperbound);
     }
 
-   //echo "\n passed third function";
-
-    function main($pArray, $nullmean){
+    function main($pArray, $nullmean, $alpha){
         $teststat = (float) getteststat($pArray, $nullmean);
-        //echo 'made it to second line';
-        echo sprintf("the test statistic is %f\n", $teststat); //prints test stat
-        //echo "made it after the test stat";
-        getconfinterval($pArray); //prints confidence interval
-        retrievepval($pArray, $teststat); //prints p value
+        printf("the test statistic is %f", $teststat);
+        //prints test stat
+        getconfinterval($pArray. $alpha); //prints confidence interval
+        $p_value = retrievepval($pArray, $teststat); //prints p value
+        printf("the p value is %f", $p_value);
+        if ($p_value < $alpha){echo "We reject the null hypothesis";}
+        else{echo "We fail to reject the null hypothesis";}
+        
     }
 }
 
