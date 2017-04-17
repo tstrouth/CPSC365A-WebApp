ec0fbd31939aabec8332855db890bc124d9e7473
Zachary A. Rodgers <zarodgers@lab-mcsp-21.academic.roanoke.edu>
2017-04-17 10:28:39 -0400

    five number summary

--- app/controllers/StatController.php
+++ app/controllers/StatController.php
@@ -23,5 +23,39 @@ class StatController extends Controller {
       }
       return (sqrt($sum /(count($an_array) - 1)));
   }
+
+  public function findMedian($anArray){
+      $median = (double) 0;
+      $half = count($anArray) / 2;
+      if (count($anArray)%2 == 0){
+          $median = (double) ($anArray[$half] + $anArray[$half - 1]) / 2; 
+      }
+      else{
+          $median = (double) $anArray[$half];
+      }
+      return $median
+  }
+  public function fiveNumber($anArray){
+      $sorted = sort($anArray);
+      $lower = $sorted[0];
+      $upper = $sorted[count($sorted) - 1];
+      $half = count($sorted) / 2;
+      $left = [];
+      $right = [];
+      for ($index = 0; $index < $half; $index++){
+          $left[$index] = $sorted[$index];
+          if (count($sorted) % 2 == 0){
+              $right[$index] = $sorted[$index + $half];
+          }
+          else{
+              $right[$index] = $sorted[$index + $half+ 1];
+          }
+      }
+      $median = findMedian($sorted);
+      $q1 = findMedian($left);
+      $q3 = findMedian($right);
+      $fiveNumber = [$lower, $q1, $median, $q3, $upper];
+      return $fiveNumber;
+  }
   
 }
