450bc7f1273a227b54c8b2b8770cb65a7fca26c6
Tyler Strouth <tcstrouth@lab-mcsp-04.academic.roanoke.edu>
2017-04-22 10:44:23 -0400

    modified StatController to include functions for paired t-test and included comments for clarity

--- app/controllers/StatController.php
+++ app/controllers/StatController.php
@@ -6,7 +6,6 @@ class StatController extends Controller {
 	{
 		return View::make('hello');
 	}
-
     //needs an array and inputted test statisitc executes a python program and
     //returns the probability value
     function retrievepval($pArray, $teststat){
@@ -29,28 +28,49 @@ class StatController extends Controller {
     //calculates the confidence interval of the population data
     function getconfinterval($pArray, $alpha){
         $standard_deviation_div_sqrtn = (std_dev($pArray)/sqrt(count($pArray)));
-        $alpha_div_2 = $alpha/2;
-        $command = sprintf("python3 get_tval.py %f %f", $alpha_div_2,
-        count($pArray) - 1);
+        $alpha_div_2 = (float) $alpha/2;
+        $df = count($pArray) - 1;
+        $command = sprintf("python3 get_tval.py %f %d", $alpha_div_2, $df);
         exec($command, $t);
         $mean = mean($pArray);
         $lowerbound = $mean - $t[0]*$standard_deviation_div_sqrtn;
         $upperbound = $mean + $t[0]*$standard_deviation_div_sqrtn;
-        printf("the confidence interval is [ %f, %f]",
-        $lowerbound, $upperbound);
+        printf("the confidence interval is [ %f, %f] <br>", $lowerbound,
+        $upperbound);
     }
 
-    function main($pArray, $nullmean, $alpha){
-        $teststat = (float) getteststat($pArray, $nullmean);
-        printf("the test statistic is %f", $teststat);
-        //prints test stat
-        getconfinterval($pArray. $alpha); //prints confidence interval
-        $p_value = retrievepval($pArray, $teststat); //prints p value
-        printf("the p value is %f", $p_value);
-        if ($p_value < $alpha){echo "We reject the null hypothesis";}
-        else{echo "We fail to reject the null hypothesis";}
-        
+    //computes the sum of ((x_x -x\bar) -((yi-y\bar)) ^ 2
+    //part of the matched pairs t-test formula 
+    function matched_pairs_std_dev($pArray1, $pArray2){
+        $sum = 0;
+        $mean1 = mean($pArray1);
+        $mean2 = mean($pArray2);
+        for ($index = 0; $index < count($pArray2); $index++) {
+            $x_i = ($pArray1[$index] - $mean1);
+            $y_i = ($pArray2[$index] - $mean2);
+            $sum = $sum + ($x_i - $y_i)**2;
+        }
+        return $sum;
+    
     }
+
+    //computes the test statistic for the paired t-test
+    function matched_pairs_test_stat($pArray1, $pArray2){
+        $m1 = mean($pArray1);
+        $m2 = mean($pArray2);
+        $n = count($pArray2);
+        $tt = (($m1 - $m2)*
+        sqrt(($n*($n-1))/matched_pairs_std_dev($pArray1,$pArray2)));
+        return $tt;
+    }
+
+    //retrieves the p-value for the paired t-test
+    function matched_pairs_p_val($pArray1, $pArray2){
+        $tt = matched_pairs_test_stat($pArray1, $pArray2);
+        $p_val = retrievepval($pArray2, $tt);
+        return $p_val;
+    }
+    
 }
 
 
