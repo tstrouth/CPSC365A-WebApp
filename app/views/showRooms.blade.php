d1adff9335fd4ebc1e612b05d7db1d8eaeb50d6f
Josh Storo <joshstoro@Joshs-MacBook-Pro.local>
2017-04-26 23:53:32 -0400

    Javascript to inject basic values

--- app/views/showRooms.blade.php
+++ app/views/showRooms.blade.php
@@ -27,7 +27,8 @@
       $("#mean").html(data.mean);
       $("#std-dev").html(data.std_dev);
       $("#median").html(data.median);
-      $("#five-number").html(data.five_number[0] + ", " + data.five_number[1] + ", " data.five_number[2] + ", " + data.five_number[3] + ", " + data.five_number[4]);
+      var five_string = data.five_number[0] + ", " + data.five_number[1] + ", " data.five_number[2] + ", " + data.five_number[3] + ", " + data.five_number[4];
+      $("#five-number").html(five_string);
       $("#data-table").html(data.histo);
     });
     updateStatistical();
