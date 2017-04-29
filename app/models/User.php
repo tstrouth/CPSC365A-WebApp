1711ffc94c325a0dda6fcdd6e6599e6965a92d8c
cssmith <chssmith@roanoke.edu>
2017-04-29 19:48:03 -0400

    Fixing the API
    Modified the API to correct errors

--- app/models/User.php
+++ app/models/User.php
@@ -3,18 +3,18 @@
 class User extends Eloquent {
 
 
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'Users';
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'Users';
 
-	public $timestamps = false;
-	protected $primaryKey = 'ID';
+    public $timestamps = false;
+    protected $primaryKey = 'ID';
 
-	public function type(){
-    return $this->hasOne("UserType", "ID", "user_type");
-  }
+    public function type(){
+	return $this->hasOne("UserType", "ID", "user_type");
+    }
 
 }
