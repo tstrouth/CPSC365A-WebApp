5f2dbac1b1d61858dacc6f0778dda266febf838e
Tyler Strouth <tstrouth@users.noreply.github.com>
2019-01-16 12:02:21 -0500

    Update get_tval.py

--- app/controllers/get_tval.py
+++ app/controllers/get_tval.py
@@ -1,8 +1,8 @@
-#Given an alpha/2 value and degrees of freedom will return a t-value
-import sys
 from scipy.stats import t
-alpha_div_2 = float(sys.argv[1])
-df = float(sys.argv[2])
-t = t.ppf(alpha_div_2, df)
+import sys
 
-print(t)
+alpha = float(sys.argv[1])
+
+df = float(sys.argv[2])
+
+print(abs(t.ppf(alpha, df)))
